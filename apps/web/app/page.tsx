b34951ba6dcd7b5c1b667d4025bcb69eb78e4861
Piorek1994 <pprus94@gmail.com>
2024-03-14 22:12:47 +0100

    fixed files for build

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,49 +1,71 @@
-'use client';
+"use client";
 
 import { useState } from "react";
+import "./style.css";
 
 export default function Page(): JSX.Element {
   const [formData, setFormData] = useState({
-    heading: '',
-    username: '',
-    content: '',
-    gender: 'male',
-    inviteLink: '',
-    inviteLinkContent: '',
-  })
+    heading: "",
+    username: "",
+    content: "",
+    gender: "male",
+    inviteLink: "",
+    inviteLinkContent: "",
+  });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
     setFormData({
       ...formData,
-      [e.target.id]: e.target.value
-    })
-  }
+      [e.target.id]: e.target.value,
+    });
+  };
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    window.location.replace(`/mailing?username=${formData.username}&content=${formData.content}&gender=${formData.gender}&inviteLink=${formData.inviteLink}&inviteLinkContent=${formData.inviteLinkContent}&heading=${formData.heading}`);
-  }
+    window.location.replace(
+      `/mailing?username=${formData.username}&content=${formData.content}&gender=${formData.gender}&inviteLink=${formData.inviteLink}&inviteLinkContent=${formData.inviteLinkContent}&heading=${formData.heading}`
+    );
+  };
 
   return (
     <div>
       Create email from provided forms...
-
       <div>
         <form onSubmit={handleSubmit}>
-
-        <div>
+          <div>
             <label htmlFor="heading">Title of email</label>
-            <input onChange={handleChange} type="text" id="heading" name="heading" required />
+            <input
+              onChange={handleChange}
+              type="text"
+              id="heading"
+              name="heading"
+              required
+            />
           </div>
 
           <div>
             <label htmlFor="username">User name</label>
-            <input onChange={handleChange} type="text" id="username" name="username" required />
+            <input
+              onChange={handleChange}
+              type="text"
+              id="username"
+              name="username"
+              required
+            />
           </div>
 
           <div>
             <label htmlFor="content">Content</label>
-            <textarea onChange={handleChange} id="content" name="content" required />
+            <textarea
+              onChange={handleChange}
+              id="content"
+              name="content"
+              required
+            />
           </div>
 
           <div>
@@ -57,12 +79,22 @@ export default function Page(): JSX.Element {
 
           <div>
             <label htmlFor="inviteLink">URL to redirect</label>
-            <input onChange={handleChange} id="inviteLink" name="inviteLink" required />
+            <input
+              onChange={handleChange}
+              id="inviteLink"
+              name="inviteLink"
+              required
+            />
           </div>
 
           <div>
             <label htmlFor="inviteLinkContent">URL Button content</label>
-            <input onChange={handleChange} id="inviteLinkContent" name="inviteLinkContent" required />
+            <input
+              onChange={handleChange}
+              id="inviteLinkContent"
+              name="inviteLinkContent"
+              required
+            />
           </div>
 
           <button type="submit">Submit</button>
